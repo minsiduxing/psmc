a6f727b020cfa96fff0d6a7dd751359c86fd344d
汪磊 <bingxuewulei@outlook.com>
2017-09-22 21:55:49 +0800

    脚本提交

--- db/PSMC脚本/【1】表创建脚本.sql
+++ db/PSMC脚本/【1】表创建脚本.sql
@@ -45,3 +45,45 @@ ALTER TABLE tab_role_resource ADD CONSTRAINT fk_tab_role_fk8_tab_role FOREIGN KE
 ALTER TABLE tab_role_resource ADD CONSTRAINT fk_tab_role_fk9_tab_reso FOREIGN KEY (resource_id) REFERENCES tab_resource (uuid);
 
 
+--创建模块相关表
+----模块表
+DROP TABLE IF EXISTS `tab_module`;
+CREATE TABLE `tab_module` (
+  `uuid` varchar(100) NOT NULL,
+  `create_acc_uuid` varchar(100) DEFAULT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `modify_acc_uuid` varchar(100) DEFAULT NULL,
+  `modify_date` timestamp NULL DEFAULT NULL,
+  `audit` decimal(10,0) DEFAULT NULL,
+  `audit_acc_uuid` varchar(100) DEFAULT NULL,
+  `audit_date` timestamp NULL DEFAULT NULL,
+  `release_acc_uuid` varchar(100) DEFAULT NULL,
+  `release_date` timestamp NULL DEFAULT NULL,
+  `one_level_classify` varchar(20) DEFAULT NULL COMMENT '1、新闻。2、微课程、3、视频',
+  `two_level_classify` varchar(20) DEFAULT NULL COMMENT '参考数据字典two_level classify',
+  `release_status` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+---模块发布表
+DROP TABLE IF EXISTS `tab_module_publish`;
+CREATE TABLE `tab_module_publish` (
+  `publish_uuid` varchar(100) NOT NULL,
+  `module_uuid` varchar(100) DEFAULT NULL,
+  `publish_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '1是\r\n            2否',
+  `publish_expire_date` timestamp NULL DEFAULT NULL,
+  `publish_account_uuid` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`publish_uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-----新闻相关表
+DROP TABLE IF EXISTS `tab_news`;
+CREATE TABLE `tab_news` (
+  `uuid` varchar(100) NOT NULL,
+  `news_title` varchar(400) DEFAULT NULL,
+  `news_subtitle` varchar(300) DEFAULT NULL,
+  `news_content` varchar(3000) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `news_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `news_author` varchar(200) DEFAULT NULL,
+  `thumbnail_image_url` varchar(200) DEFAULT NULL,
+  `news_abstract` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
