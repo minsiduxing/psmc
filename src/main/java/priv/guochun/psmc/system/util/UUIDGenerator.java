c4d08a6baf5b4e4607c0810b40aed54e6a3a0bb6
minsiduxing <minsiduxing@163.com>
2022-12-28 10:55:41 +0800

    解决打包问题

--- src/main/java/priv/guochun/psmc/system/util/UUIDGenerator.java
+++ src/main/java/priv/guochun/psmc/system/util/UUIDGenerator.java
@@ -2,6 +2,8 @@ package priv.guochun.psmc.system.util;
 
 import org.apache.commons.jexl3.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class UUIDGenerator {   
@@ -25,26 +27,19 @@ public class UUIDGenerator {
         for (int i = 0; i < number; i++) {   
             ss[i] = createUUID();   
         }
-            JexlBuilder jexlBuilder = new JexlBuilder();
-            // 创建Jexl表达式引擎
-            JexlEngine jexlEngine = jexlBuilder.create();
-            // 创建Jexl表达式解析器
-            JexlScript jexlScript = jexlEngine.createScript("if(GRID_PEOPLE_COUNT<1000 || (GRID_PEOPLE_COUNT>=1000 && ACTUAL_ISSUE_CERT_TOTAL<5)){ if(GRID_PEOPLE_COUNT/200-ACTUAL_ISSUE_CERT_TOTAL>0){result=true}else{result=false} } else{ if((GRID_PEOPLE_COUNT-1000)/500-(ACTUAL_ISSUE_CERT_TOTAL-5)>0){result=true}else{result=false} }");
-//        JexlScript jexlScript = jexlEngine.createScript("result=GRID_PEOPLE_COUNT/200");
-        // 创建Jexl表达式变量上下文
-            JexlContext jexlContext = new MapContext();
-            jexlContext.set("GRID_PEOPLE_COUNT", 1999);
-            jexlContext.set("ACTUAL_ISSUE_CERT_TOTAL", 5);
-            // 执行Jexl表达式，得到结果
-            jexlScript.execute(jexlContext);
-            System.out.println(jexlContext.get("result"));
         return ss;
-    }    
+    }
   
     public static void main(String[] args) {   
         String[] ss = createUUIDs(5);   
         for (int i = 0; i < ss.length; i++) {   
             System.out.println("ss["+i+"]====="+ss[i]);   
-        }   
+        }
+
+        Map<String,String> map = new HashMap<String,String>();
+        map.put("GRID_PEOPLE_COUNT", "1999");
+        map.put("ACTUAL_ISSUE_CERT_TOTAL", "6");
+        boolean reusult = JetlUtil.execute("if(GRID_PEOPLE_COUNT<1000 || (GRID_PEOPLE_COUNT>=1000 && ACTUAL_ISSUE_CERT_TOTAL<5)){ if(GRID_PEOPLE_COUNT/200-ACTUAL_ISSUE_CERT_TOTAL>0){result=true}else{result=false} } else{ if((GRID_PEOPLE_COUNT-1000)/500-(ACTUAL_ISSUE_CERT_TOTAL-5)>0){result=true}else{result=false} }",map);
+        System.out.println(reusult);
     }   
 }  
