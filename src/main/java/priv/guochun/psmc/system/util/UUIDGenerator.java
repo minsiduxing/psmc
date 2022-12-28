675fa04fb1fb6ced496c019fe8fbb7629c3c8638
minsiduxing <minsiduxing@163.com>
2022-12-28 09:51:13 +0800

    增加jetl表达式支持

--- src/main/java/priv/guochun/psmc/system/util/UUIDGenerator.java
+++ src/main/java/priv/guochun/psmc/system/util/UUIDGenerator.java
@@ -1,5 +1,7 @@
 package priv.guochun.psmc.system.util;
 
+import org.apache.commons.jexl3.*;
+
 import java.util.UUID;
 
 public class UUIDGenerator {   
@@ -22,8 +24,21 @@ public class UUIDGenerator {
         String[] ss = new String[number];   
         for (int i = 0; i < number; i++) {   
             ss[i] = createUUID();   
-        }   
-        return ss;   
+        }
+            JexlBuilder jexlBuilder = new JexlBuilder();
+            // 创建Jexl表达式引擎
+            JexlEngine jexlEngine = jexlBuilder.create();
+            // 创建Jexl表达式解析器
+            JexlScript jexlScript = jexlEngine.createScript("if(GRID_PEOPLE_COUNT<1000 || (GRID_PEOPLE_COUNT>=1000 && ACTUAL_ISSUE_CERT_TOTAL<5)){ if(GRID_PEOPLE_COUNT/200-ACTUAL_ISSUE_CERT_TOTAL>0){result=true}else{result=false} } else{ if((GRID_PEOPLE_COUNT-1000)/500-(ACTUAL_ISSUE_CERT_TOTAL-5)>0){result=true}else{result=false} }");
+//        JexlScript jexlScript = jexlEngine.createScript("result=GRID_PEOPLE_COUNT/200");
+        // 创建Jexl表达式变量上下文
+            JexlContext jexlContext = new MapContext();
+            jexlContext.set("GRID_PEOPLE_COUNT", 1999);
+            jexlContext.set("ACTUAL_ISSUE_CERT_TOTAL", 5);
+            // 执行Jexl表达式，得到结果
+            jexlScript.execute(jexlContext);
+            System.out.println(jexlContext.get("result"));
+        return ss;
     }    
   
     public static void main(String[] args) {   
